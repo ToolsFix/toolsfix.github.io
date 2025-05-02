2aa4018dda51042c82bb37f9524d1eb0124a06d2
ToolsFix <141168667+ToolsFix@users.noreply.github.com>
2025-05-02 16:31:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const stuffs = [
 const esigns = [ 
     {
         name: 'ESign Installer',
-        url: 'itms-services://?action=download-manifest&url=https://ipa.ipasign.cc:2052/sign/20250502162745384/7b2c0c38-5bd5-c118-d6fb-24d3e172eac4/app.plist"',
+        url: 'itms-services://?action=download-manifest&url=https://ipa.ipasign.cc:2052/sign/20250502173033570/353af023-a34e-15cd-33b7-c83f59b8f92a/app.plist"',
         img: 'img/apps/esign.png',
         descriptions: 'HDFC Bank Limited'
     }
